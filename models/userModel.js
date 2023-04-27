9e2c17fb4e974a032db12e3a7e39728b35b5418e
Ayo Moses <ayomoses@afrimash.com>
2023-04-27 05:55:57 +0100

    added necessary fields in the users model

--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,51 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, 'A user must have a first name'],
-    maxLength: [
-      40,
-      'A Users firstname must have less or equal than 40 characters',
-    ],
-    minLength: [3, 'A users name must have more or equal than 3 characters'],
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'A user must have a first name'],
+      maxLength: [
+        40,
+        'A Users firstname must have less or equal than 40 characters',
+      ],
+      minLength: [3, 'A users name must have more or equal than 3 characters'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'A user must have a first name'],
+      maxLength: [
+        40,
+        'A Users firstname must have less or equal than 40 characters',
+      ],
+      minLength: [3, 'A users name must have more or equal than 10 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter a valid email'],
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: [true, 'Please enter a valid phone number'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: [true, 'A user must have a first name'],
-    maxLength: [
-      40,
-      'A Users firstname must have less or equal than 40 characters',
-    ],
-    minLength: [3, 'A users name must have more or equal than 10 characters'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Please enter a valid email'],
-    unique: true,
-  },
-  phone: {
-    type: String,
-    required: [true, 'Please enter a valid phone number'],
-    unique: true,
-  },
-  username: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
