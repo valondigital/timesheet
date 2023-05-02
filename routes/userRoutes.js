04d5f5f5676f1edcb8d9e91b0b7493ab45e10ed4
Ayo Moses <ayomoses111@gmail.com>
2023-05-02 20:15:03 +0100

    resolved limiter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -23,7 +23,7 @@ const {
 const router = express.Router();
 
 const limiter = rateLimit({
-  max: 10,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour',
 });
