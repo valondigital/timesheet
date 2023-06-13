341e61bada5a5e97a7fc21f403aec95974ebf7d7
Ayo Moses <ayomoses@afrimash.com>
2023-06-14 00:11:53 +0100

    Add folder for server

---
+++ server/models/taskModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'inProgress', 'completed'],
+      default: 'pending',
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
