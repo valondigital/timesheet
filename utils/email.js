f1bfaa665e001546fdcb025cb8ea38bcbcd07aaf
Ayo Moses <ayomoses111@gmail.com>
2023-06-20 17:27:02 +0100

    Remove API key

--- utils/email.js
+++ utils/email.js
@@ -14,24 +14,24 @@ module.exports = class Email {
 
   newTransport() {
     // if (process.env.NODE_ENV === 'production') {
-    // const transporter = nodemailer.createTransport({
-    //   host: 'smtp-relay.sendinblue.com',
-    //   port: 587,
-    //   auth: {
-    //     user: 'ayomoses111@gmail.com',
-    //     pass: 'xsmtpsib-69491b3f0bb7c7a8ea50deaec757eb7d87906ae9197f63d727240b62d38b8881-HNSWYvyQ9nsCBwz7',
-    //   },
-    // });
-    // return transporter;
-    // }
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+    const transporter = nodemailer.createTransport({
+      host: 'smtp-relay.sendinblue.com',
+      port: 587,
       auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_API_KEY,
       },
     });
+    return transporter;
+    // }
+    // return nodemailer.createTransport({
+    //   host: process.env.EMAIL_HOST,
+    //   port: process.env.EMAIL_PORT,
+    //   auth: {
+    //     user: process.env.EMAIL_USERNAME,
+    //     pass: process.env.EMAIL_PASSWORD,
+    //   },
+    // });
   }
 
   async send(template, subject) {
