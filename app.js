32572582a46165ea6fbdf243b186c6fe3db01ba2
Ayo Moses <ayomoses111@gmail.com>
2023-06-23 00:21:42 +0100

    Resolve CORS error

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const hpp = require('hpp');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
@@ -11,6 +12,8 @@ const roleRouter = require('./routes/roleRoutes');
 const deptRouter = require('./routes/departmentRoutes');
 
 const app = express();
+
+app.use(cors());
 
 // Global Middlewares
 
@@ -40,6 +43,7 @@ app.use('/api/v1/departments', deptRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
 
 app.use(globalErrorHandler);
 
