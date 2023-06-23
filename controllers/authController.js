186b3c53f3c46191d7149fa854927cfbb1e68b4c
Ayo Moses <ayomoses111@gmail.com>
2023-06-23 12:44:24 +0100

    Work on the token expiration

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,7 +20,8 @@ const generateRandomPassword = (length = 8) => {
 
 const signToken = (id) => {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    // expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: 5 * 24 * 60 * 60,
   });
   return token;
 };
